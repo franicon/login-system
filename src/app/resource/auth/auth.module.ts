31d945003901f22e7c30ee86cce8c8dfcf2b93ac
Abayomi Aluko <franicons@gmail.com>
2022-11-09 21:44:50 +0100

    feat: setup Database Connection #14
    * Setup Database Connection
    * Install mongoose
    * set up schema the data
    * and Joi Validation
    * setup mongodb directory
    
    refs: #14

--- src/app/resource/auth/auth.module.ts
+++ src/app/resource/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongoModule } from '../../provider/mongodb/_.module';
 
 @Module({
+  imports: [MongoModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
